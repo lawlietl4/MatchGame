0cd3494b51b589292562dcf5a779d60a65fbc767
lawlietl4 <nroberson7712@gmail.com>
2021-05-13 19:28:11 -0600

    added more stuff

--- obj/Debug/net5.0-windows/Controls/Card.g.i.cs
+++ obj/Debug/net5.0-windows/Controls/Card.g.i.cs
@@ -70,13 +70,13 @@ namespace MatchGame.Controls {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MatchGame;component/controls/card.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MatchGame;V1.0.0.0;component/controls/card.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Controls\Card.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -86,7 +86,7 @@ namespace MatchGame.Controls {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
